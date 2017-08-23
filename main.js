9216dc33c18e4ed7f26699763760e0e5217a68c0
pmc2 <Philip.Collins@unh.edu>
2017-08-23 11:25:09 -0400

    more

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ document.getElementById('PauseBut').onclick=()=>{
 
 
 // keyboard controller
+let KEYBOARD_CONTROLLER;
 { let c={ x:0, y:0, space:0 };
   window.addEventListener("keydown", (e)=>{
     switch(e.keyCode) {
@@ -34,7 +35,7 @@ document.getElementById('PauseBut').onclick=()=>{
       case 40: c.y=0; break; // down
     }
   });
-  window.KEYBOARD_CONTROLLER = c;
+  KEYBOARD_CONTROLLER=c;
 }
 
 
@@ -58,145 +59,152 @@ STAGE.update=()=>{
   STAGE.y -= ((STAGE.y - ((PLAYER.y - (RENDERER.height/2)) * -1)) / (RENDERER.height/2)) * ELAPSED_TIME;
 }
 
-
+// make ground
 let GROUND = [];
-let makeGround=(x,y,w,h,color)=>{
-  let g = new PIXI.Graphics();
-  if (! x) x=0;
-  if (! y) y=0;
-  if (! w) w=200;
-  if (! h) w=20;
-  g.beginFill(color);
-  g.drawRect(0, 0, w, h);
-  g.endFill();
-  g.x=x;
-  g.y=y;
-  g.climbable = false;
-  g.update=()=>{};
-  STAGE.addChild(g);
-  GROUND.push(g);
+{ let m=(x,y,w,h,color)=>{
+    let g = new PIXI.Graphics();
+    if (! x) x=0;
+    if (! y) y=0;
+    if (! w) w=200;
+    if (! h) w=20;
+    g.beginFill(color);
+    g.drawRect(0, 0, w, h);
+    g.endFill();
+    g.x=x;
+    g.y=y;
+    g.climbable = false;
+    g.update=()=>{};
+    STAGE.addChild(g);
+    GROUND.push(g);
+  }
+  m(40,100,50,1,0x4f844e);
+  m(100,200,90,1,0x4f844e);
+  m(50,300,400,1,0x4f844e);
+  m(80,400,80,1,0x4f844e);
+  m(10,440,800,1,0x4f844e);
 }
-makeGround(40,100,50,1,0x4f844e);
-makeGround(100,200,90,1,0x4f844e);
-makeGround(50,300,400,1,0x4f844e);
-makeGround(80,400,80,1,0x4f844e);
-makeGround(10,440,800,1,0x4f844e);
 
-
-let PLAYER = new PIXI.Graphics();
-STAGE.addChild(PLAYER);
-PLAYER.beginFill(0x0e47a3);
-PLAYER.lineStyle(1, 0x000000, 1);
-PLAYER.drawRect(0, 0, 4, 20);
-PLAYER.endFill();
-PLAYER.ground = null;
-PLAYER.x = PLAYER.prevX = 100;
-PLAYER.y = PLAYER.prevY = 100;
-PLAYER.speed_x = 0;
-PLAYER.speed_y = 0;
-PLAYER.acceleration_x = .0003;
-PLAYER.stop_acceleration_x = .0008;
-PLAYER.acceleration_y = .0004;
-PLAYER.stop_acceleration_y = .0008;
-PLAYER.fall_acceleration_y = .001;
-PLAYER.fall_acceleration_x = .0003;
-PLAYER.max_speed_x = 1;
-PLAYER.max_speed_y = 1;
-PLAYER.jump_speed = .2;
-PLAYER.jump_end = 0;
-PLAYER.controller = window.KEYBOARD_CONTROLLER;
-PLAYER.update=()=>{
-  if (PLAYER.ground) {
-    // handle x movement (walking)
-    if (PLAYER.controller.x==1 && PLAYER.speed_x >= 0) {
-      PLAYER.speed_x += PLAYER.acceleration_x * ELAPSED_TIME;
-    } else if (PLAYER.controller.x==-1 && PLAYER.speed_x <= 0) {
-      PLAYER.speed_x -= PLAYER.acceleration_x * ELAPSED_TIME;
-    } else if (PLAYER.speed_x > 0) {
-      PLAYER.speed_x -= PLAYER.stop_acceleration_x * ELAPSED_TIME;
-      if (PLAYER.speed_x < 0) PLAYER.speed_x=0;
-    } else if (PLAYER.speed_x < 0) {
-      PLAYER.speed_x += PLAYER.stop_acceleration_x * ELAPSED_TIME;
-      if (PLAYER.speed_x > 0) PLAYER.speed_x=0;
-    }
-
-    // handle y movement (climbing)
-    if (PLAYER.controller.y==1 && PLAYER.speed_y >= 0) {
-      PLAYER.speed_y += PLAYER.acceleration_y * ELAPSED_TIME;
-    } else if (PLAYER.controller.y==-1 && PLAYER.speed_y <= 0) {
-      PLAYER.speed_y -= PLAYER.acceleration_y * ELAPSED_TIME;
-    } else if (PLAYER.speed_y > 0) {
-      PLAYER.speed_y -= PLAYER.stop_acceleration_y * ELAPSED_TIME;
-      if (PLAYER.speed_y < 0) PLAYER.speed_y=0;
-    } else if (PLAYER.speed_y < 0) {
-      PLAYER.speed_y += PLAYER.stop_acceleration_y * ELAPSED_TIME;
-      if (PLAYER.speed_y > 0) PLAYER.speed_y=0;
-    }
-
-    // handle jump
-    if (PLAYER.controller.space) {
-      PLAYER.ground=null;
-      PLAYER.jump_end = T1 + 1000;
-      PLAYER.speed_y = -.3;
-    }
-  }
-
-  // handle falling
-  else {
-    PLAYER.speed_y += PLAYER.fall_acceleration_y * ELAPSED_TIME; 
-
-    // handle x movement
-    if (PLAYER.controller.x==1 && PLAYER.speed_x >= 0) {
-      PLAYER.speed_x += PLAYER.fall_acceleration_x * ELAPSED_TIME;
-    } else if (PLAYER.controller.x==-1 && PLAYER.speed_x <= 0) {
-      PLAYER.speed_x -= PLAYER.fall_acceleration_x * ELAPSED_TIME;
-    }
-
-    if (PLAYER.controller.space) {
-      if (T1 > 0 && T1 < PLAYER.jump_end) {
-        PLAYER.speed_y -= .01;
+let createPlayer=()=>{
+  let p = new PIXI.Graphics();
+  STAGE.addChild(p);
+  p.beginFill(0x0e47a3);
+  p.lineStyle(1, 0x000000, 1);
+  p.drawRect(0, 0, 4, 20);
+  p.endFill();
+  p.ground = null;
+  p.x = p.prevX = 100;
+  p.y = p.prevY = 100;
+  p.speed_x = 0;
+  p.speed_y = 0;
+  p.acceleration_x = .0008;
+  p.acceleration_y = .0008;
+  p.stop_acceleration_x = .001;
+  p.stop_acceleration_y = .0008;
+  p.fall_acceleration_y = .002;
+  p.fall_acceleration_x = .0006;
+  p.max_speed_x = 1;
+  p.max_speed_y = 1;
+  p.start_jump_speed = .5;
+  p.continue_jump_speed = .001;
+  p.continue_jump_ms = 900;
+  p.jump_end = 0;
+  p.controller = null;
+  p.update=()=>{
+    if (p.ground) {
+      // handle x movement (walking)
+      if (p.controller.x==1 && p.speed_x >= 0) {
+        p.speed_x += p.acceleration_x * ELAPSED_TIME;
+      } else if (p.controller.x==-1 && p.speed_x <= 0) {
+        p.speed_x -= p.acceleration_x * ELAPSED_TIME;
+      } else if (p.speed_x > 0) {
+        p.speed_x -= p.stop_acceleration_x * ELAPSED_TIME;
+        if (p.speed_x < 0) p.speed_x=0;
+      } else if (p.speed_x < 0) {
+        p.speed_x += p.stop_acceleration_x * ELAPSED_TIME;
+        if (p.speed_x > 0) p.speed_x=0;
       }
-    }
-  }
-
-  // handle maximum veolcities
-  if (PLAYER.speed_x > PLAYER.max_speed_x)
-    PLAYER.speed_x=PLAYER.max_speed_x;
-  else if (PLAYER.speed_x < -1*PLAYER.max_speed_x)
-    PLAYER.speed_x=PLAYER.max_speed_x*-1;
-  if (PLAYER.speed_y > PLAYER.max_speed_y)
-    PLAYER.speed_y=PLAYER.max_speed_y;
-  else if (PLAYER.speed_y < -1*PLAYER.max_speed_y)
-    PLAYER.speed_y=PLAYER.max_speed_y*-1;
   
-  // update position
-  PLAYER.prevX = PLAYER.x;
-  PLAYER.prevY = PLAYER.y;
-  PLAYER.x += PLAYER.speed_x * ELAPSED_TIME;
-  PLAYER.y += PLAYER.speed_y * ELAPSED_TIME;
-
-  // is the ground still valid
-  if (PLAYER.ground && (PLAYER.x < PLAYER.ground.x || (PLAYER.x > PLAYER.ground.x + PLAYER.ground.width))) {
-    PLAYER.ground=null;
-  }
-
-  // handle ground
-  if (PLAYER.ground) {
-    PLAYER.y = PLAYER.ground.y-PLAYER.height;
-    PLAYER.speed_y=0;
-  } else {
-
-    // look for a ground
-    for (let g of GROUND) {
-      if ((PLAYER.x >= g.x && PLAYER.x <= (g.x + g.width)) && 
-          (PLAYER.prevY+PLAYER.height <= g.y && PLAYER.y+PLAYER.height >= g.y)) {
-        PLAYER.ground = g;
-        break;
+      // handle y movement (climbing)
+      if (p.controller.y==1 && p.speed_y >= 0) {
+        p.speed_y += p.acceleration_y * ELAPSED_TIME;
+      } else if (p.controller.y==-1 && p.speed_y <= 0) {
+        p.speed_y -= p.acceleration_y * ELAPSED_TIME;
+      } else if (p.speed_y > 0) {
+        p.speed_y -= p.stop_acceleration_y * ELAPSED_TIME;
+        if (p.speed_y < 0) p.speed_y=0;
+      } else if (p.speed_y < 0) {
+        p.speed_y += p.stop_acceleration_y * ELAPSED_TIME;
+        if (p.speed_y > 0) p.speed_y=0;
+      }
+  
+      // handle jump
+      if (p.controller.space) {
+        p.ground=null;
+        p.jump_end = T1 + p.continue_jump_ms;
+        p.speed_y = p.start_jump_speed * -1;
+      }
+    }
+  
+    // handle falling
+    else {
+      p.speed_y += p.fall_acceleration_y * ELAPSED_TIME; 
+  
+      // handle x movement
+      if (p.controller.x==1 && p.speed_x >= 0) {
+        p.speed_x += p.fall_acceleration_x * ELAPSED_TIME;
+      } else if (p.controller.x==-1 && p.speed_x <= 0) {
+        p.speed_x -= p.fall_acceleration_x * ELAPSED_TIME;
+      }
+  
+      if (p.controller.space) {
+        if (T1 > 0 && T1 < p.jump_end) {
+          p.speed_y -= (p.continue_jump_speed * ELAPSED_TIME);
+        }
+      }
+    }
+  
+    // handle maximum veolcities
+    if (p.speed_x > p.max_speed_x)
+      p.speed_x=p.max_speed_x;
+    else if (p.speed_x < -1*p.max_speed_x)
+      p.speed_x=p.max_speed_x*-1;
+    if (p.speed_y > p.max_speed_y)
+      p.speed_y=p.max_speed_y;
+    else if (p.speed_y < -1*p.max_speed_y)
+      p.speed_y=p.max_speed_y*-1;
+    
+    // update position
+    p.prevX = p.x;
+    p.prevY = p.y;
+    p.x += p.speed_x * ELAPSED_TIME;
+    p.y += p.speed_y * ELAPSED_TIME;
+  
+    // is the ground still valid
+    if (p.ground && (p.x < p.ground.x || (p.x > p.ground.x + p.ground.width))) {
+      p.ground=null;
+    }
+  
+    // handle ground
+    if (p.ground) {
+      p.y = p.ground.y-p.height;
+      p.speed_y=0;
+    } else {
+  
+      // look for a ground
+      for (let g of GROUND) {
+        if ((p.x >= g.x && p.x <= (g.x + g.width)) && 
+            (p.prevY+p.height <= g.y && p.y+p.height >= g.y)) {
+          p.ground = g;
+          break;
+        }
       }
     }
   }
-};
-
+  return p;
+}
+let PLAYER=createPlayer();
+PLAYER.controller=KEYBOARD_CONTROLLER;
+  
 
 
 let MSG = new PIXI.Text("", {
