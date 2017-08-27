086afc00e57540f7798da4a00795e5af3dadd112
pmc2 <Philip.Collins@unh.edu>
2017-08-27 12:08:43 -0400

    controller pause support

--- main.js
+++ main.js
@@ -13,10 +13,8 @@ let PAUSE_MSG = new PIXI.Text("PAUSED", {
   dropShadow: true,
   dropShadowBlur: 10
 });
-let tooglePause=()=>{
+let togglePause=()=>{
   if (IS_PAUSED) {
-    T0=performance.now();
-    requestAnimationFrame(GAME_LOOP);
     IS_PAUSED=false;
     SCREEN.removeChild(PAUSE_MSG);
   } else {
@@ -28,6 +26,20 @@ let tooglePause=()=>{
   }
 }
 
+// use input mask to store the state of buttons
+let
+  BUT_UP     =1<<0,
+  BUT_DOWN   =1<<1,
+  BUT_LEFT   =1<<2,
+  BUT_RIGHT  =1<<3,
+  BUT_RUN    =1<<4,
+  BUT_JUMP   =1<<5,
+  BUT_FIRE   =1<<6,
+  BUT_SHEILD =1<<7,
+  BUT_MISSILE=1<<8,
+  BUT_BOMB   =1<<9,
+  BUT_START  =1<<10,
+  BUT_SELECT =1<<11;
 
 
 // gamepad and keyboard controllers
@@ -40,21 +52,22 @@ let updateGamepads=()=>{
       if (gamePadPlayerIdxMap[i]==undefined) {
         gamePadPlayerIdxMap[i]=createPlayer();
       }
-      let c = PLAYERS[gamePadPlayerIdxMap[i]].controller;
-      let b=gps[i].buttons;
-      c.up     =b[12].pressed;
-      c.down   =b[13].pressed;
-      c.left   =b[14].pressed;
-      c.right  =b[15].pressed;
-      c.run    =b[ 2].pressed;
-      c.jump   =b[ 0].pressed;
-      c.fire   =b[ 7].pressed;
-      c.sheild =b[ 6].pressed;
-      c.missile=b[ 5].pressed;
-      c.bomb   =b[ 4].pressed;
-      c.down   =b[13].pressed;
-      c.start  =b[ 9].pressed;
-      c.select =b[ 8].pressed;
+      let p = PLAYERS[gamePadPlayerIdxMap[i]];
+      let b = gps[i].buttons;
+      p.input =  
+        (b[12].pressed && BUT_UP) |
+        (b[13].pressed && BUT_DOWN) |
+        (b[14].pressed && BUT_LEFT) |
+        (b[15].pressed && BUT_RIGHT) |
+        (b[ 2].pressed && BUT_RUN) |
+        (b[ 0].pressed && BUT_JUMP) |
+        (b[ 7].pressed && BUT_FIRE) |
+        (b[ 6].pressed && BUT_SHEILD) |
+        (b[ 5].pressed && BUT_MISSILE) |
+        (b[ 4].pressed && BUT_BOMB) |
+        (b[13].pressed && BUT_DOWN) |
+        (b[ 9].pressed && BUT_START) |
+        (b[ 8].pressed && BUT_SELECT);
     }
 
     // else inactive
@@ -69,38 +82,54 @@ let updateGamepads=()=>{
 let keyboardPlayerIdx=null;
 window.addEventListener("keydown", (e)=>{
 //console.log(e.keyCode);
+  let k = e.keyCode;
 
   // if esc button
-  if (e.keyCode==27) {
-    tooglePause(); 
+  if (k==27) {
+    togglePause(); 
     return;
   }
-  if (keyboardPlayerIdx===null && e.keyCode >=37 && e.keyCode <= 40) {
+  if (keyboardPlayerIdx===null && k>=37 && k<=40) {
     keyboardPlayerIdx=createPlayer();
     console.log('created new player for keyboard');
   }
   if (keyboardPlayerIdx===null) return;
-  let c = PLAYERS[keyboardPlayerIdx].controller;
-  switch(e.keyCode) {
-    case 16: c.run  =true; break; // shift
-    case 32: c.jump =true; break; // spacebar
-    case 37: c.left =true; break; // left
-    case 38: c.up   =true; break; // up
-    case 39: c.right=true; break; // right
-    case 40: c.down =true; break; // down
-  }
+
+  let p = PLAYERS[keyboardPlayerIdx];
+  p.input |=  
+    (k==38 && BUT_UP)      ||
+    (k==40 && BUT_DOWN)    ||
+    (k==37 && BUT_LEFT)    ||
+    (k==39 && BUT_RIGHT)   ||
+    (k==16 && BUT_RUN)     ||
+    (k==32 && BUT_JUMP)    ||
+    (k== 0 && BUT_FIRE)    ||
+    (k== 0 && BUT_SHEILD)  ||
+    (k== 0 && BUT_MISSILE) ||
+    (k== 0 && BUT_BOMB)    ||
+    (k== 0 && BUT_DOWN)    ||
+    (k== 0 && BUT_START)   ||
+    (k== 0 && BUT_SELECT);
 });
+
 window.addEventListener("keyup", (e)=>{
   if (keyboardPlayerIdx===null) return;
-  let c = PLAYERS[keyboardPlayerIdx].controller;
-  switch(e.keyCode) {
-    case 16: c.run  =false; break; // shift
-    case 32: c.jump =false; break; // spacebar
-    case 37: c.left =false; break; // left
-    case 38: c.up   =false; break; // up
-    case 39: c.right=false; break; // right
-    case 40: c.down =false; break; // down
-  }
+  let k = e.keyCode;
+  let p = PLAYERS[keyboardPlayerIdx];
+  p.input &= ~( 
+    (k==38 && BUT_UP)      ||
+    (k==40 && BUT_DOWN)    ||
+    (k==37 && BUT_LEFT)    ||
+    (k==39 && BUT_RIGHT)   ||
+    (k==16 && BUT_RUN)     ||
+    (k==32 && BUT_JUMP)    ||
+    (k== 0 && BUT_FIRE)    ||
+    (k== 0 && BUT_SHEILD)  ||
+    (k== 0 && BUT_MISSILE) ||
+    (k== 0 && BUT_BOMB)    ||
+    (k== 0 && BUT_DOWN)    ||
+    (k== 0 && BUT_START)   ||
+    (k== 0 && BUT_SELECT) );
 });
 
 
@@ -201,114 +230,124 @@ let createPlayer=()=>{
     var i = PLAYERS.indexOf(p);
     if (i!=-1) PLAYERS.splice(i, 1);
   };
-  p.controller={up:false,down:false,left:false,right:false,run:false,jump:false,fire:false,sheild:false,missile:false,bomb:false,start:false,select:false};
-  p.update=()=>{
-    if (p.ground) {
 
-      // handle walk/run x axis movement
-      if (p.controller.right && p.speed_x >= 0) {
-        if (p.controller.run) {
-          p.speed_x += p.run_acceleration_x * ELAPSED_TIME;
+  p.input=0;
+  p.prevInput=0;
+  p.update=()=>{
+
+    // if player hit pause key
+    if (p.input & BUT_START && !(p.prevInput & BUT_START)) {
+      togglePause();
+    }
+
+    if (! IS_PAUSED) {
+      if (p.ground) {
+  
+        // handle walk/run x axis movement
+        if (p.input & BUT_RIGHT && p.speed_x >= 0) {
+          if (p.input & BUT_RUN) {
+            p.speed_x += p.run_acceleration_x * ELAPSED_TIME;
+            if (p.speed_x > p.run_max_speed_x) {
+              p.speed_x=p.run_max_speed_x;
+            }
+          } else {
+            p.speed_x += p.walk_acceleration_x * ELAPSED_TIME;
+            if (p.speed_x > p.walk_max_speed_x) {
+              p.speed_x=p.walk_max_speed_x;
+            }
+          }
+        } else if (p.input & BUT_LEFT && p.speed_x <= 0) {
+          if (p.input & BUT_RUN) {
+            p.speed_x -= p.run_acceleration_x * ELAPSED_TIME;
+            if (p.speed_x < (p.run_max_speed_x * -1)) {
+              p.speed_x=p.run_max_speed_x * -1;
+            }
+          } else {
+            p.speed_x -= p.walk_acceleration_x * ELAPSED_TIME;
+            if (p.speed_x < (p.walk_max_speed_x * -1)) {
+              p.speed_x=p.walk_max_speed_x * -1;
+            }
+          }
+        } else if (p.speed_x > 0) {
+          p.speed_x -= p.stop_acceleration_x * ELAPSED_TIME;
+          if (p.speed_x < 0) p.speed_x=0;
+        } else if (p.speed_x < 0) {
+          p.speed_x += p.stop_acceleration_x * ELAPSED_TIME;
+          if (p.speed_x > 0) p.speed_x=0;
+        }
+  
+        // handle y movement (climbing)
+        if (p.input & BUT_DOWN && p.speed_y >= 0) {
+          p.speed_y += p.walk_acceleration_y * ELAPSED_TIME;
+        } else if (p.input & BUT_UP && p.speed_y <= 0) {
+          p.speed_y -= p.walk_acceleration_y * ELAPSED_TIME;
+        } else if (p.speed_y > 0) {
+          p.speed_y -= p.stop_acceleration_y * ELAPSED_TIME;
+          if (p.speed_y < 0) p.speed_y=0;
+        } else if (p.speed_y < 0) {
+          p.speed_y += p.stop_acceleration_y * ELAPSED_TIME;
+          if (p.speed_y > 0) p.speed_y=0;
+        }
+    
+        // handle jump
+        if (p.input & BUT_JUMP && !(p.prevInput & BUT_JUMP)) {
+          p.ground=null;
+          p.jump_end = T1 + p.continue_jump_ms;
+          p.speed_y = p.start_jump_speed * -1;
+        }
+      }
+    
+      // handle falling
+      else {
+        p.speed_y += p.fall_acceleration_y * ELAPSED_TIME; 
+    
+        // handle x movement
+        if (p.input & BUT_RIGHT && p.speed_x >= 0) {
+          p.speed_x += p.fall_acceleration_x * ELAPSED_TIME;
           if (p.speed_x > p.run_max_speed_x) {
             p.speed_x=p.run_max_speed_x;
           }
-        } else {
-          p.speed_x += p.walk_acceleration_x * ELAPSED_TIME;
-          if (p.speed_x > p.walk_max_speed_x) {
-            p.speed_x=p.walk_max_speed_x;
-          }
-        }
-      } else if (p.controller.left && p.speed_x <= 0) {
-        if (p.controller.run) {
-          p.speed_x -= p.run_acceleration_x * ELAPSED_TIME;
-          if (p.speed_x < (p.run_max_speed_x * -1)) {
+        } else if (p.input & BUT_LEFT && p.speed_x <= 0) {
+          p.speed_x -= p.fall_acceleration_x * ELAPSED_TIME;
+          if (p.speed_x < p.run_max_speed_x * -1) {
             p.speed_x=p.run_max_speed_x * -1;
           }
-        } else {
-          p.speed_x -= p.walk_acceleration_x * ELAPSED_TIME;
-          if (p.speed_x < (p.walk_max_speed_x * -1)) {
-            p.speed_x=p.walk_max_speed_x * -1;
+        }
+    
+        if (p.input & BUT_JUMP) {
+          if (T1 > 0 && T1 < p.jump_end) {
+            p.speed_y -= (p.continue_jump_speed * ELAPSED_TIME);
           }
         }
-      } else if (p.speed_x > 0) {
-        p.speed_x -= p.stop_acceleration_x * ELAPSED_TIME;
-        if (p.speed_x < 0) p.speed_x=0;
-      } else if (p.speed_x < 0) {
-        p.speed_x += p.stop_acceleration_x * ELAPSED_TIME;
-        if (p.speed_x > 0) p.speed_x=0;
-      }
-
-      // handle y movement (climbing)
-      if (p.controller.down && p.speed_y >= 0) {
-        p.speed_y += p.walk_acceleration_y * ELAPSED_TIME;
-      } else if (p.controller.up && p.speed_y <= 0) {
-        p.speed_y -= p.walk_acceleration_y * ELAPSED_TIME;
-      } else if (p.speed_y > 0) {
-        p.speed_y -= p.stop_acceleration_y * ELAPSED_TIME;
-        if (p.speed_y < 0) p.speed_y=0;
-      } else if (p.speed_y < 0) {
-        p.speed_y += p.stop_acceleration_y * ELAPSED_TIME;
-        if (p.speed_y > 0) p.speed_y=0;
       }
   
-      // handle jump
-      if (p.controller.jump) {
+      // update position
+      p.prevX = p.x;
+      p.prevY = p.y;
+      p.x += p.speed_x * ELAPSED_TIME;
+      p.y += p.speed_y * ELAPSED_TIME;
+  
+      // is the ground still valid
+      if (p.ground && (p.x < p.ground.x || (p.x > p.ground.x + p.ground.width))) {
         p.ground=null;
-        p.jump_end = T1 + p.continue_jump_ms;
-        p.speed_y = p.start_jump_speed * -1;
-      }
-    }
-  
-    // handle falling
-    else {
-      p.speed_y += p.fall_acceleration_y * ELAPSED_TIME; 
-  
-      // handle x movement
-      if (p.controller.right && p.speed_x >= 0) {
-        p.speed_x += p.fall_acceleration_x * ELAPSED_TIME;
-        if (p.speed_x > p.run_max_speed_x) {
-          p.speed_x=p.run_max_speed_x;
-        }
-      } else if (p.controller.left && p.speed_x <= 0) {
-        p.speed_x -= p.fall_acceleration_x * ELAPSED_TIME;
-        if (p.speed_x < p.run_max_speed_x * -1) {
-          p.speed_x=p.run_max_speed_x * -1;
-        }
       }
   
-      if (p.controller.jump) {
-        if (T1 > 0 && T1 < p.jump_end) {
-          p.speed_y -= (p.continue_jump_speed * ELAPSED_TIME);
+      // handle ground
+      if (p.ground) {
+        p.y = p.ground.y-p.height;
+        p.speed_y=0;
+      } else {
+        // look for a ground
+        for (let g of GROUND) {
+          if ((p.x >= g.x && p.x <= (g.x + g.width)) && 
+              (p.prevY+p.height <= g.y && p.y+p.height >= g.y)) {
+            p.ground = g;
+            break;
+          }
         }
       }
-    }
-  
-    // update position
-    p.prevX = p.x;
-    p.prevY = p.y;
-    p.x += p.speed_x * ELAPSED_TIME;
-    p.y += p.speed_y * ELAPSED_TIME;
-  
-    // is the ground still valid
-    if (p.ground && (p.x < p.ground.x || (p.x > p.ground.x + p.ground.width))) {
-      p.ground=null;
-    }
-  
-    // handle ground
-    if (p.ground) {
-      p.y = p.ground.y-p.height;
-      p.speed_y=0;
-    } else {
-  
-      // look for a ground
-      for (let g of GROUND) {
-        if ((p.x >= g.x && p.x <= (g.x + g.width)) && 
-            (p.prevY+p.height <= g.y && p.y+p.height >= g.y)) {
-          p.ground = g;
-          break;
-        }
-      }
-    }
+    } 
+    p.prevInput=p.input;
   }
 
   let i = PLAYERS.length;
@@ -351,6 +390,6 @@ let GAME_LOOP=(t1)=>{
   SCREEN.update();
   RENDERER.render(SCREEN);
   T0=T1;
-  if (! IS_PAUSED) requestAnimationFrame(GAME_LOOP);
+  requestAnimationFrame(GAME_LOOP);
 };
 GAME_LOOP(T0);
